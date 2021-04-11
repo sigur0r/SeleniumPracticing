4ccd33a088f4f9d83f752a4dae6b65b11a5a2f5d
Artyom Schukin <37814017+sigur0r@users.noreply.github.com>
2021-04-11 10:41:57 +0700

    Created all basic files

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                         help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en")
